129b25cd82122ce9468175f4e9cc74a1f53103bd
Josue Lopez <josuereach2446@gmail.com>
2025-05-30 01:21:34 -0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Josue  Lopez  on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
